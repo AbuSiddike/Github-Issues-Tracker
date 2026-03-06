c963748638337b44710e1e51ab4cfb9084369a1c
AbuSiddike <asiddike18@gmail.com>
2026-03-06 21:28:37 +0600

    loginUI complete with js interactivity

---
+++ scripts/login.js
@@ -0,0 +1,18 @@
+function login() {
+
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+if(username === "admin" && password === "admin123"){
+
+localStorage.setItem("isLoggedIn", true);
+
+window.location.href = "index.html";
+
+}else{
+
+alert("Invalid credentials");
+
+}
+
+}
