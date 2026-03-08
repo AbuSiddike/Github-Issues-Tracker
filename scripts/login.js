442c04f2f425d1ed1cc043cc859b3de142f264df
AbuSiddike <asiddike18@gmail.com>
2026-03-08 08:23:18 +0600

    validation, functionality, conditions updated for login

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,61 @@
-function login() {
+const usernameInput = document.getElementById("usernameInput");
+const passwordInput = document.getElementById("passwordInput");
 
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
+const userHint = document.getElementById("userHint");
+const passHint = document.getElementById("passHint");
 
-if(username === "admin" && password === "admin123"){
+const loginBtn = document.getElementById("loginBtn");
 
-localStorage.setItem("isLoggedIn", true);
+loginBtn.addEventListener("click", function () {
 
-window.location.href = "index.html";
+  userHint.innerText = "";
+  passHint.innerText = "";
 
-}else{
+  const usernameValue = usernameInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
 
-alert("Invalid credentials");
+  // username validation
+  if (usernameValue === "") {
+    userHint.innerHTML =
+      `<i class="fa-regular fa-circle-xmark"></i> Please enter <span class="text-green-500">'admin'</span> your user name`;
+    userHint.classList.add("text-red-500", "animate-bounce");
+    return;
+  } 
+  else if (usernameValue !== "admin") {
+    userHint.innerHTML =
+      `<i class="fa-regular fa-circle-xmark"></i> Wrong user name. Please enter 'admin'`;
+    userHint.classList.add("text-red-500", "animate-bounce");
+    return;
+  } 
+  else {
+    userHint.classList.remove("text-red-500", "animate-bounce", "text-purple-500");
+    userHint.classList.add("text-green-500");
+    userHint.innerHTML =
+      `<i class="fa-regular fa-circle-check"></i> Correct Your user name`;
+  }
 
-}
+  // password validation
+  if (passwordValue === "") {
+    passHint.innerHTML =
+      `<i class="fa-regular fa-circle-xmark"></i> Please enter <span class="text-green-500">'admin123'</span> your password`;
+    passHint.classList.add("text-red-500", "animate-bounce");
+    return;
+  } 
+  else if (passwordValue !== "admin123") {
+    passHint.innerHTML =
+      `<i class="fa-regular fa-circle-xmark"></i> Wrong password. Please enter 'admin123'`;
+    passHint.classList.add("text-red-500", "animate-bounce");
+    return;
+  } 
+  else {
+    passHint.classList.remove("text-red-500", "animate-bounce", "text-purple-500");
+    passHint.classList.add("text-green-500");
+    passHint.innerHTML =
+      `<i class="fa-regular fa-circle-check"></i> Correct Your Password`;
 
-}
+    // redirect after successful login
+    window.location.href = "./home.html";
+  }
+
+  console.log(usernameValue, passwordValue);
+});
